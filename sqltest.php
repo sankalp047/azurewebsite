5572ed459e3cea2e0574b4e134c326b9d2e98fb6
sankalp047 <38282659+sankalp047@users.noreply.github.com>
2022-03-05 20:28:49 -0600

    Update sqltest.php

--- sqltest.php
+++ sqltest.php
@@ -7,13 +7,15 @@
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT * FROM earthquake.eq;";
-    $getResults= sqlsrv_query($conn, $tsql);
-    echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
-    }
-    sqlsrv_free_stmt($getResults);
+    if ($conn==TRUE)
+        echo("Connected!");
+    //$tsql= "SELECT * FROM earthquake.eq;";
+    //$getResults= sqlsrv_query($conn, $tsql);
+    //echo ("Reading data from table" . PHP_EOL);
+    //if ($getResults == FALSE)
+    //    echo (sqlsrv_errors());
+    //while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    // echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+  //  }
+   // sqlsrv_free_stmt($getResults);
 ?>
