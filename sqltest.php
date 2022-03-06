ffba90fd7ab11c082fb64dc2b6cdd3de99e80b18
sankalp047 <38282659+sankalp047@users.noreply.github.com>
2022-03-05 18:58:30 -0600

    Create sqltest.php

---
+++ sqltest.php
@@ -0,0 +1,19 @@
+<?php
+    $serverName = "mysqlassign.mysql.database.azure.com"; // update me
+    $connectionOptions = array(
+        "Database" => "earthquake", // update me
+        "Uid" => "mysql@mysqlassign", // update me
+        "PWD" => "qwerty@123" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT * FROM earthquake.eq;";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
